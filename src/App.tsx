8ff1a0e9c788e137bd96e961433cdb0e7994a433
PIQON <naskret.kamil@wp.pl>
2023-05-30 18:26:16 +0200

    feat: add react-router config to App component, init pages components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import { Register } from './pages/FormPages/Register/Register';
+import { Login } from './pages/FormPages/Login/Login';
+import { NotFound } from './pages/NotFound/NotFound';
+import { Main } from './pages/Main/Main';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+]);
+
 export const App = () => {
-  return <h1>Husky Testing ASD!</h1>;
+  return <RouterProvider router={router} />;
 };

b33db80d0e22030970254e5b31c56f6f28914cb6
PIQON <naskret.kamil@wp.pl>
2023-06-01 16:55:13 +0200

    feat: add a main hero typography with styling, change a hero image, work with translations

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,22 +1,25 @@
 import style from './Main.module.scss';
 import { Wrapper } from '../../features/UI/Wrapper/Wrapper';
 import { MainNavigation } from './MainNavigation/MainNavigation';
+import { useTranslation } from 'react-i18next';
 
 export const Main = () => {
+  const { t } = useTranslation();
   return (
     <div className={style['main']}>
-      <header className={style['main__header']}>
-        <Wrapper maxWidth="60rem">
+      <header className={style['header']}>
+        <Wrapper maxWidth="70rem">
           <MainNavigation />
         </Wrapper>
-        <div>
-          <h1>Filmy, seriale i wiele więcej bez ograniczeń</h1>
-          <p>Oglądaj wszędzie. Anuluj w każdej chwili.</p>
-          <p>
-            Zaczynamy oglądać? Wprowadź adres e‑mail, aby utworzyć lub odnowić
-            konto.
-          </p>
-        </div>
+        <Wrapper maxWidth="55rem">
+          <div className={style['header__content']}>
+            <h1 className={style['header__title']}>{t('main.title')}</h1>
+            <p className={style['header__description']}>
+              {t('main.description')}
+            </p>
+            <p className={style['header__addition']}>{t('main.addition')}</p>
+          </div>
+        </Wrapper>
       </header>
     </div>
   );
